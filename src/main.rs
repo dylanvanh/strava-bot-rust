2551a6cc200e664135313d5f1f57998f5ddbcfb0
Dylan van Heerden <61427891+dylanvanh@users.noreply.github.com>
2025-08-24 09:57:06 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use crate::clients::strava::StravaClient;
+
+mod clients;
+
+fn main() {
+    println!("Hello, world!");
+
+    // let Ok(strava_client) =
+    //     StravaClient::new("asd".to_string(), "asd".to_string(), "asd".to_string())
+    // else {
+    //     return;
+    // };
+    //
+    // print!(strava_client);
+}
